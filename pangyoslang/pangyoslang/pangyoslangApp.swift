c24a58859949eb928bf402295e0717c46aa9c2c8
kihanam <ifdikei@gmail.com>
2026-05-20 12:00:38 +0900

    setting: 리드미와 프로젝트 파일 생성

---
+++ pangyoslang/pangyoslang/pangyoslangApp.swift
@@ -0,0 +1,17 @@
+//
+//  pangyoslangApp.swift
+//  pangyoslang
+//
+//  Created by 케이 on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct pangyoslangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
